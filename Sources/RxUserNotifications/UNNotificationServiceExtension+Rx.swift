dd0196a67ebd076ebace0a5e2f68a0f283415c25
Alex Mavrichev <mz@mac7.ru>
2021-12-18 22:10:52 +0100

    Update UNNotificationServiceExtension+Rx.swift

--- Sources/RxUserNotifications/UNNotificationServiceExtension+Rx.swift
+++ Sources/RxUserNotifications/UNNotificationServiceExtension+Rx.swift
@@ -10,7 +10,16 @@ import RxSwift
 import RxCocoa
 
 @available(iOS 10.0, watchOS 6.0, macOS 10.14, *)
-extension Reactive where Base: UNNotificationServiceExtension {
+extension UNNotificationServiceExtension {
+  static var rx: RxStatic.Type {
+    RxStatic.self
+  }
+
+  struct RxStatic {}
+}
+
+@available(iOS 10.0, watchOS 6.0, macOS 10.14, *)
+extension UNNotificationServiceExtension.RxStatic {
 
 	/// Asks you to make any needed changes to the notification and notify the system when you're done.
 	/// - Parameter request: The original notification request. Use this object to get the original content of the notification.
