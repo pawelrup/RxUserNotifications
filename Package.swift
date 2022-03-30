3fcfeeab36c096578439eef9919b12be1e289f21
Alex Mavrichev <mz@mac7.ru>
2022-03-30 12:01:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-		.package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMinor(from: "6.0.0"))
+		.package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
